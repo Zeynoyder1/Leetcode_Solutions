020b9866d4660db49892d8958c69097f238e1520
Zeynoyder1 <129191416+Zeynoyder1@users.noreply.github.com>
2024-08-12 00:33:10 -0700

    update

---
+++ 41_First_Missing_Number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        ans = 1
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] == ans:
+                ans+=1
+                i+=1
+        return ans
+
